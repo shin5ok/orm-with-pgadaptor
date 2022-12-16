a94af75fb5a6af5c6d6abb46f406aadee302b903
shin5ok <shin5ok@55mp.com>
2022-12-17 03:11:20 +0900

    specify the table definition manually because of autoload not working

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ import click
 import os
 import json
 import logging
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import *
 
 DSN: str = os.environ.get("DSN", "postgresql://@localhost/game")
@@ -16,6 +16,13 @@ logging.basicConfig()
 loggingConfig = logging.getLogger('sqlalchemy.engine')
 if debug_flag:
     loggingConfig.setLevel(logging.DEBUG)
+
+users = Table('users', MetaData(bind=engine),
+            Column('user_id', String(32), primary_key=True),
+            Column('name', String(32)),
+            Column('created_at', TIMESTAMP(timezone=False)),
+            Column('updated_at', TIMESTAMP(timezone=False)),
+        )
 
 @click.group()
 def cli() -> None:
@@ -29,12 +36,10 @@ def put(name: str) -> None:
 def writing(name: str) -> None:
     import uuid
 
-    users = Table("users", MetaData(bind=engine), autoload=True)
-
     try:
         with engine.begin() as connection:
             user_id = str(uuid.uuid4())
-            dt = datetime.now()
+            dt = datetime.now(timezone.utc)
             r = {"user_id": user_id, "name": name, "created_at": dt, "updated_at":dt}
             connection.execute(users.insert(), r)
     except Exception as e:
