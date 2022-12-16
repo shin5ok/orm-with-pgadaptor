2050a52c92c0cfe8e5d7529e3823b007ee8cbfde
shin5ok <shin5ok@55mp.com>
2022-12-16 14:12:15 +0900

    work it well on real postgresql

---
+++ db.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from sqlalchemy import *
+import click
+import os
+import json
+import logging
+from typing import *
+
+DSN: str = os.environ.get("DSN", "postgresql://@localhost/game")
+engine = create_engine(DSN)
+
+debug_flag: bool = "DEBUG" in os.environ
+logging.basicConfig()
+loggingConfig = logging.getLogger('sqlalchemy.engine')
+if debug_flag:
+    loggingConfig.setLevel(logging.DEBUG)
+
+@click.group()
+def cli() -> None:
+    pass
+
+@cli.command()
+@click.option("--name", "-n")
+def put(name: str) -> None:
+    writing(name) 
+
+def writing(name: str) -> None:
+    import uuid
+
+    users = Table("users", MetaData(bind=engine), autoload=True)
+
+    try:
+        with engine.begin() as connection:
+            user_id = str(uuid.uuid4())
+            import datetime
+            dt = datetime.datetime.now()
+            connection.execute(users.insert(), {"user_id": user_id, "name": name, "created_at": dt, "updated_at":dt})
+    except Exception as e:
+        print(str(e))
+
+if __name__ == '__main__':
+    cli()
